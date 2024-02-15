94d5ca4621deed2ddd991767fbe2c55fed528242
arman <shakerian.arman@yahoo.com>
2024-02-15 19:15:21 +0330

    feat: file-size-validator

---
+++ QGPT/QuestionAnswer/validators.py
@@ -0,0 +1,12 @@
+from django.core.exceptions import ValidationError
+from django.template.defaultfilters import filesizeformat
+from .validators import file_size_validator
+
+
+def file_size_validator(value, max_size=None):
+    one_megabyte = 1048576 
+    
+    if max_size is None: # set 10-megabytes as max size default
+        max_size = one_megabyte * 10
+    if value.size > max_size:
+        raise ValidationError(f"File size shouldn't exceed {filesizeformat(max_size)}")
