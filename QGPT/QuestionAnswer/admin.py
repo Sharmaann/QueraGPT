c5b4439f76c0c7fecfbeeb66cba8e9d8f7fe6902
arman <shakerian.arman@yahoo.com>
2024-02-16 16:21:55 +0330

    feat: register answer model

--- QGPT/QuestionAnswer/admin.py
+++ QGPT/QuestionAnswer/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Answer
 
 # Register your models here.
 
 
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
     pass
