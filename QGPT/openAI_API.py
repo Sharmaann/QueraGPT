f3c8dde1818ece676c0849055b3bd83ce0db5e1d
arman <shakerian.arman@yahoo.com>
2024-02-17 12:26:13 +0330

    initial opeanAI API

---
+++ QGPT/openAI_API.py
@@ -0,0 +1,27 @@
+from dotenv import load_dotenv
+from openai import OpenAI, api_key
+import os
+
+
+load_dotenv()
+
+api_key = os.environ.get("OPENAI_API_KEY")
+
+
+client = OpenAI()
+
+completion = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {
+            "role": "system",
+            "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair.",
+        },
+        {
+            "role": "user",
+            "content": "Compose a poem that explains the concept of recursion in programming.",
+        },
+    ],
+)
+
+print(completion.choices[0].message)
