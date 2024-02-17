de72915994ca232716d2148dd9f3999908bb6acf
arman <shakerian.arman@yahoo.com>
2024-02-17 10:34:41 +0330

    feat: add problem, answer + fix: add question

--- QGPT/QuestionAnswer/views.py
+++ QGPT/QuestionAnswer/views.py
@@ -2,11 +2,11 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 
-from .models import Question, Answer
-from .forms import QuestionForm
+from .models import Question, Answer, Problem
+from .forms import QuestionForm, AnswerForm, ProblemForm
 
 
-def get_question_title(request):
+def get_question_title(request):  # TODO
     if request.method == "GET":
         question_id = request.GET.get("question_id")
         try:
@@ -21,17 +21,54 @@ def get_question_title(request):
 def add_question(request):
     if request.method == "GET":
         form = QuestionForm()
-        context = {"question_form": form}
-        return render(request, "question_add.html", context)
+        context = {"form": form}
+        return render(request, "add_form.html", context)
 
     if request.method == "POST":  # TODO
         form = QuestionForm(request.POST, request.FILES)
-        context = {"question_form": form}
         if form.is_valid():
             data = form.cleaned_data
-            question = Question()
             Question.objects.create(**data)
             return HttpResponse("Question added", status=201)
+        return HttpResponse("Error", status=400)
+
+    return HttpResponse("Method not allowed")
+
+
+# REFACTOR: code duplication
+
+
+@csrf_exempt
+def add_answer(request):
+    if request.method == "GET":
+        form = AnswerForm()
+        context = {"form": form}
+        return render(request, "add_form.html", context)
+
+    if request.method == "POST":  # TODO
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            Question.objects.create(**data)
+            return HttpResponse("Answer added", status=201)
+        return HttpResponse("Error", status=400)
+
+    return HttpResponse("Method not allowed")
+
+
+@csrf_exempt
+def add_problem(request):
+    if request.method == "GET":
+        form = ProblemForm()
+        context = {"form": form}
+        return render(request, "add_form.html", context)
+
+    if request.method == "POST":  # TODO
+        form = ProblemForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            Problem.objects.create(**data)
+            return HttpResponse("Problem added", status=201)
         return HttpResponse("Error", status=400)
 
     return HttpResponse("Method not allowed")
