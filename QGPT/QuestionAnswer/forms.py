e86e2a8fe86cb3c8b905861ae04304c51e2d6cf0
arman <shakerian.arman@yahoo.com>
2024-02-17 10:34:10 +0330

    add problem, answer form

--- QGPT/QuestionAnswer/forms.py
+++ QGPT/QuestionAnswer/forms.py
@@ -1,9 +1,20 @@
 from django import forms
 
-from .models import Question
+from .models import Question, Answer, Problem
 
 
 class QuestionForm(forms.ModelForm):
     class Meta:
         model = Question
+        fields = "__all__"
+
+
+class AnswerForm(forms.ModelForm):
+    class Meta:
+        model = Answer
+        fields = "__all__"
+        
+class ProblemForm(forms.ModelForm):
+    class Meta:
+        model = Problem
         fields = "__all__"
